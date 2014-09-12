b0ff50e6225dc96f398d79a69ea81b5b52a4dea7
Chris Anderson <grommett@me.com>
2014-09-12 19:09:15 +1000

    Comment change

--- OSX_CommandLine1/main.m
+++ OSX_CommandLine1/main.m
@@ -14,7 +14,7 @@ int main(int argc, const char * argv[])
     @autoreleasepool {
         
         // insert code here...
-        NSLog(@"Hello, World!");
+        NSLog(@"Hello, World from GitHub and Chris!");
         
     }
     return 0;
