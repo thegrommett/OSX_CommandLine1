a61e445b4ec767c4353df4f0fc0c1dacc37e5404
Chris Anderson <grommett@me.com>
2014-09-12 18:55:06 +1000

    Initial commit from local account

---
+++ OSX_CommandLine1/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  OSX_CommandLine1
+//
+//  Created by Chris Anderson on 12/09/2014.
+//  Copyright (c) 2014 Chris Anderson. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
